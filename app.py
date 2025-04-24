9f57f181ee0bb063b577835aa8d8071d89212ef1
Sanjeev <sanjeevsnair1412@gmail.com>
2025-04-24 17:19:58 +0530

    c

--- app.py
+++ app.py
@@ -590,4 +590,4 @@ if __name__ == "__main__":
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=8080)
 
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=8000)
